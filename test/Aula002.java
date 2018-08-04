f6363c1b30b359d81a2d6372d598e3c1687bf365
NicholasNeto <npnicolls@gmail.com>
2018-08-04 20:38:16 -0300

    criado teste para aula 2

--- test/Aula002.java
+++ test/Aula002.java
@@ -1,13 +1,17 @@
 
 import com.nicholas.estruturadados.vetor.Vetor;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class Aula002 {
 
-    public static void main (String[] args){
+    @Test
+    public void deveCriarUmaListaComCapacidadeIndicada(){
+        Vetor vetor = new  Vetor(6);
 
-        Vetor vetor = new Vetor(3);
-
+        assertEquals(6, vetor.capacidade());
     }
 
 }
