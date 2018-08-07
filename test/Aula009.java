a6ea714808ade22df028508db3e69fb2be612da4
NicholasNeto <npnicolls@gmail.com>
2018-08-07 15:56:15 -0300

    criando test para metodo de remover elemento

--- test/Aula009.java
+++ test/Aula009.java
@@ -1,4 +1,44 @@
-package PACKAGE_NAME;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.nicholas.estruturadados.vetor.Vetor;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 public class Aula009 {
+
+    Vetor vetor;
+
+
+    @BeforeEach
+    public void setUp() {
+
+        vetor = new Vetor(5);
+
+        vetor.add("A");
+        vetor.add("B");
+        vetor.add("C");
+        vetor.add("D");
+    }
+
+
+    @Test
+    public void deveRemoverUmElementoDaLista(){
+
+        assertEquals(4 , vetor.tamanho());
+        vetor.remove(1);
+        assertEquals(3 , vetor.tamanho());
+    }
+
+
+    @Test
+    public void deveRemoverUmElementoEspecifico(){
+
+        vetor.tamanho();
+        int posicao = vetor.busca("A");
+        vetor.remove(posicao);
+
+        assertEquals(3, vetor.tamanho());
+    }
 }
