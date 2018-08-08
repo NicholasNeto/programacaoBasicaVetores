8fd2f9224162a61fadb52c8f9ca5b45282033870
NicholasNeto <npnicolls@gmail.com>
2018-08-08 16:54:45 -0300

    criando teste para o exercicio 3

--- test/exercicios/testExerc03.java
+++ test/exercicios/testExerc03.java
@@ -1,4 +1,31 @@
 package exercicios;
 
+import com.nicholas.estruturadados.vetor.Lista;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class testExerc03 {
+
+    Lista list;
+
+    @BeforeEach
+    public void setUp(){
+
+        list = new Lista(6);
+
+        list.add("A");
+        list.add("B");
+        list.add("C");
+        list.add("D");
+    }
+
+    @Test
+    public void deveriaRemoverElementoEspecificoElementoPassado(){
+
+        assertEquals(4, list.tamanho());
+        list.removeByElement("A");
+        assertEquals(3, list.tamanho());
+    }
 }
