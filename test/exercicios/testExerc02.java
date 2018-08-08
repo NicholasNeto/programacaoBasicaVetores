cb633ceeea3ec82202e3bef14be1b69c905d11e2
NicholasNeto <npnicolls@gmail.com>
2018-08-08 14:25:57 -0300

    criando teste para o exercicio 2

--- test/exercicios/testExerc02.java
+++ test/exercicios/testExerc02.java
@@ -1,4 +1,41 @@
 package exercicios;
 
+import com.nicholas.estruturadados.vetor.Lista;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
 public class testExerc02 {
+
+    /*
+        Melhore a classe Lista implementando o metodo ultimo indice, semelhante ao metodo lastIndexOf da classe arrayList
+
+     */
+
+
+    Lista list;
+
+
+    @BeforeEach
+    public void setUp(){
+
+        list = new Lista(6);
+
+        list.add("A");
+        list.add("B");
+        list.add("C");
+        list.add("A");
+    }
+
+    @Test
+    public void deveRetornarOIndiceDaUltimaPosicaoDoElementoBuscado() {
+
+       assertEquals(3, list.lastIndexOf("A"));
+
+
+    }
+
+
 }
