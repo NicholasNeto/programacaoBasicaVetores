85d51682d1176f158a47079dd757c9b15133fc83
NicholasNeto <npnicolls@gmail.com>
2018-08-17 09:29:32 -0300

    criando um lista 2 para seguir exercicios

--- src/com/nicholas/estruturadados/vetor/Lista2.java
+++ src/com/nicholas/estruturadados/vetor/Lista2.java
@@ -1,4 +1,24 @@
 package com.nicholas.estruturadados.vetor;
+import com.nicholas.estruturadados.basica;
 
-public class Lista2 {
+public class Lista2<T> extends basica {
+
+
+    public Lista2 (){
+        super();
+    }
+
+    public Lista2(int capacidade) {
+        super(capacidade);
+    }
+
+
+
+    public boolean adiciona(T elemento){
+        return super.add(elemento);
+    }
+
+    public boolean adiciona(int posicao, T elemento){
+        return super.add(posicao, elemento);
+    }
 }
